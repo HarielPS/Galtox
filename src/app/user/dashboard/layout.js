996e567f8aeb34708b0b822b5f2e5063fc773b46
marlon <marlonrodriguez2b@gmail.com>
2024-05-31 02:55:00 -0600

    CI:sidebar not finished yet

--- src/app/user/dashboard/layout.js
+++ src/app/user/dashboard/layout.js
@@ -1,7 +1,19 @@
-import React from 'react'
+"use client";
+import SideBar from "@/components/Sidebar";
+import React, { useState } from 'react';
 
-export default function layout({children}) {
+export default function layout({ children }) {
+  const [visible, setVisible] = useState(false);
+  const handleVisible = () => {
+    setVisible(!visible);
+  };
   return (
-    <div>layout {children}</div>
-  )
+    <div style={{ height: "100vh" }}>
+      <SideBar visible={visible|false} handleVisible={handleVisible} />
+      <button onClick={() => setVisible(true)}> Abrir</button>
+      <div style={{ padding: "2rem" }}>
+        {children}
+      </div>
+    </div>
+  );
 }
